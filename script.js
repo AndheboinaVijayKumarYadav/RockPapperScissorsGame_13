89f613b3276f2a3c8c6314c3277b54ebb839531a
AndheboinaVijayKumarYadav <andheboinavijaykumaryadav@gmail.com>
2023-08-06 17:01:16 +0530

    added script.js
    Added logic of determining the whether player won or computer one and displaying on screen

---
+++ script.js
@@ -0,0 +1,55 @@
+// Variables
+const playerEl = document.querySelector('.playerChoice'),
+      computerEl = document.querySelector('.computerChoice'),
+      resultEl = document.querySelector('.result'),
+      rockEl = document.querySelector('.rock'),
+      paperEl = document.querySelector('.paper'),
+      scissorEl = document.querySelector('.scissor');
+
+
+// array of choices
+const choices = ['rock','paper','scissor'];
+
+
+// adding event listener
+rockEl.addEventListener('click', () => {
+    const rock = rockEl.innerText.toLowerCase();
+    let random = randomNumber();
+    let computer = choices[random];
+    displayResult(rock,computer);
+});
+paperEl.addEventListener('click',() => {
+    let random = randomNumber();
+    let computer = choices[random];
+    const paper = paperEl.innerText.toLowerCase();
+    displayResult(paper,computer);
+});
+scissorEl.addEventListener('click',() => {
+    let random = randomNumber();
+    let computer = choices[random];
+    const scissor = scissorEl.innerText.toLowerCase();
+    displayResult(scissor,computer);
+});
+
+
+function randomNumber(){
+    let randomNumber= Math.floor((Math.random()*3));
+    return randomNumber;
+}
+
+function displayResult(value1,value2){
+    playerEl.innerText = value1;
+    computerEl.innerText = value2;
+
+    if((value1 === 'rock' && value2 === 'paper') || (value1 === 'paper' && value2 === 'scissor') || (value1 === 'scissor' && value2 === 'rock')){
+        resultEl.innerText = "Loose"
+        
+    }
+    else if(value1 === value2){
+        resultEl.innerText = "Draw"
+    }
+    else{
+        resultEl.innerText = "Win";
+    }
+
+}
